44dea5fed38e4b72386d41f4eebbaa169f6e8687
JayRKyd <knowlesjordy@gmail.com>
2022-06-16 20:10:28 -0400

    Coding Challenge for 6-16-22.

---
+++ CC 2022 6-16-22/app.js
@@ -0,0 +1,12 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // dadYearsOld must be sonYearsOld * 2
+    // dadYearsOld when son is 0 is dadYearsOld - sonYearsOld
+    let difference = dadYearsOld - sonYearsOld;
+    return Math.abs(dadYearsOld - difference*2);
+  }
