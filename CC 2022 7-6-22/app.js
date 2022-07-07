716e147400e516a9e49ca31e70ed86ff918c575b
JayRKyd <knowlesjordy@gmail.com>
2022-07-06 22:02:29 -0400

    Coding Challenge for 7-6-22.

---
+++ CC 2022 7-6-22/app.js
@@ -0,0 +1,19 @@
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+const isAQuestion = (string) => {
+    const lastChar = string.length - 1 
+  
+    if(string[lastChar] === '?')
+      return true
+    
+    return false
+  }
+  
+  const ensureQuestion = (string) => {
+    if(!isAQuestion(string)){
+      return string + '?'
+    }
+    
+    return string
+  }
