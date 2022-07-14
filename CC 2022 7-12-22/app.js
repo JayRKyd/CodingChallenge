313df27d41369a995e78ce33a933a291531159d8
JayRKyd <knowlesjordy@gmail.com>
2022-07-13 20:38:42 -0400

    Coding Challenge for 7-13-22.

--- CC 2022 7-12-22/app.js
+++ CC 2022 7-12-22/app.js
@@ -4,6 +4,7 @@
 // If the sum is definitely an odd number, return Odd.
 // If the sum is definitely an even number, return Even.
 // If the sum can be either odd or even ( depending on which first number you choose ), return Either.
+
 function oddOrEven(n) {
     if (n % 2 !== 0){return "Either"}
     if ((n / 2 )  % 2 === 0){return "Even"}
